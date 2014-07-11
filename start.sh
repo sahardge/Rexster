490a05ce645e0f1663aa5e4e680688275dbf0710
sahardge <sahardge1@catamount.wcu.edu>
2014-07-11 15:02:42 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+CASSANDRA=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /cassandraSeed/node1`
+ES=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /situ/elasticsearch/host`
 IP=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /rexsterIP`
 
 
 mv config/rexster.xml config/rexster.xml.orig
 cat config/rexster.xml.orig | sed -e "/<base-uri>/s/localhost/""$IP""/" > config/rexster.xml
+mv config/rexster.xml config/rexster.xml.orig
+cat config/rexster.xml.orig | sed -e "/storage.hostname/s/127.0.0.1/""$CASSANDRA""/" > config/rexster.xml
+mv config/rexster.xml config/rexster.xml.orig
+cat config/rexster.xml.orig | sed -e"/storage.index.search.hostname/s/127.0.0.1/""$ES""/" > config/rexster.xml
 
 ./bin/rexster.sh --start
